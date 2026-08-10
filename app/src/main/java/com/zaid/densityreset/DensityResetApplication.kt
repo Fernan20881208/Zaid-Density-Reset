1e5ae58563b47d0be8a59de949d9ffcddce41896
Zaid.nvr <utegfer@gmail.com>
2026-08-10 00:10:05 -0600

    Initialize startup remote config and update managers

--- app/src/main/java/com/zaid/densityreset/DensityResetApplication.kt
+++ app/src/main/java/com/zaid/densityreset/DensityResetApplication.kt
@@ -3,13 +3,19 @@ package com.zaid.densityreset
 import android.app.Application
 import com.zaid.densityreset.license.LicenseManager
 import com.zaid.densityreset.license.ui.LicenseUiBinder
+import com.zaid.densityreset.remoteconfig.RemoteConfigManager
 import com.zaid.densityreset.shizuku.ShizukuManager
+import com.zaid.densityreset.startup.StartupCoordinator
+import com.zaid.densityreset.update.UpdateManager
 
 class DensityResetApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         ShizukuManager.initialize(this)
+        RemoteConfigManager.initialize(this)
+        UpdateManager.initialize(this)
         LicenseManager.initialize(this)
+        StartupCoordinator.initialize(this)
         LicenseUiBinder.register(this)
     }
 }
